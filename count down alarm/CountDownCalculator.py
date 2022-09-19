08355bba55bed5121eac4f2915cca9bc0f1776a4
__init__ <89521099+ofcoursenp@users.noreply.github.com>
2022-09-19 17:08:02 +0545

    Important Note
    Before running this file do:
    1 Pip install pyautogui  
    2 pip install playsound 
    install these 2 modules then only the file will run or else it wont run

---
+++ count down alarm/CountDownCalculator.py
@@ -0,0 +1,34 @@
+import time
+import pyautogui as pg
+from playsound import playsound
+
+# Do pip3 install pyautogui and pip3 install playsound in the terminal or windows powershell
+# before running this file or else it wont work
+# Count Down Alarm , It has iphone ringtone 
+
+print("DO u want to enter time in seconds or minute")
+Whatdouwant = input("Enter here > ")
+
+if Whatdouwant.lower() == "seconds":
+    acctuallsecond = pg.prompt("Enter the time u want to start the count down in seconds : ")
+    acctuallsecond = int(acctuallsecond)
+    for i in range(1,acctuallsecond+1):
+        print(i)
+        time.sleep(1)
+        i -=1
+    playsound("iphon.mp3")
+        
+if Whatdouwant.lower() == "minute" or Whatdouwant.lower() == "minutes":
+    print("Seprate the minutes by :")
+    print("Example : 8:10 > Here : is the diffrence of 8 minutes and 10 seconds")
+    minutes = pg.prompt("Enter the time u want to start the count down in minutes : ")
+    minutes_split = minutes.split(":")
+    min = int(minutes_split[0])
+    secondsofmin = min*60
+    second_index = minutes_split[1]
+    acctuallsecond = int(secondsofmin) + int(second_index)
+    for i in range(1,acctuallsecond+1):
+        print(i)
+        time.sleep(1)
+        i -=1
+    playsound("iphon.mp3")
